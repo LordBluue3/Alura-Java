528ca81c4a0a138c4af027f273324de8026b22a0
LordBluue3 <mikaelcandido71@gmail.com>
2022-04-10 16:59:01 -0300

    Maneiras de percorrer uma lista

---
+++ src/main/java/lord/curso/arraylist/Lista_0.java
@@ -0,0 +1,37 @@
+package lord.curso.arraylist;
+
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+
+public class Lista_0 {
+    public static void main(String[] args) {
+        String aula = "Conhecendo mais as listas";
+        String aula2 = "Modelando a classe Listas_0";
+        String aula3 = "Trabalhando com cursos e sets";
+
+        ArrayList<String> aulas = new ArrayList<>();
+        //adicionando itens na lista
+        aulas.add(aula);
+        aulas.add(aula2);
+        aulas.add(aula3);
+        System.out.println(aulas);
+
+        //Maneiras de Percorrer uma lista
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("Primeira maneira de percorrer uma lista ForEach");
+        for (String aulao: aulas) {
+            System.out.println("Aula: "+ aulao);
+        }
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("Segunda maneira de percorrer uma lista For");
+        for(int j = 0; j < aulas.size(); j++){
+            System.out.println("aula: "+j+" - "+aulas.get(j));
+        }
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("Terceira maneira de percorrer uma lista método ForEach com expressão lambda");
+
+        aulas.forEach(aulaaa -> {
+            System.out.println("aula: " + aulaaa);
+        });
+    }
+}
